e7c634a93cc8eff1d1146f02c5cd47fdd4420484
JINYEONG0402 <gjy1614@naver.com>
2025-12-19 14:50:33 +0900

    health_check + build

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-for i in {1..10}
+for i in {1..60}
 do
-  STATUS=$(curl -s http://localhost:8080/actuator/health | grep UP)
+  RESPONSE=$(curl -s -w "\n%{http_code}" http://localhost:8080/actuator/health)
+  BODY=$(echo "$RESPONSE" | head -n 1)
+  CODE=$(echo "$RESPONSE" | tail -n 1)
 
-  if [ -n "$STATUS" ]; then
-    echo "Application is healthy"
+  if [ "$CODE" = "200" ] && echo "$BODY" | grep -q '"status":"UP"'; then
+     echo "Application is healthy"
     exit 0
   fi
 
