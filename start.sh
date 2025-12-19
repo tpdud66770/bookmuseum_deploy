30ee314eb08541f2bc318db543104f9116487543
MinJoon1994 <rlaalswns199@gmail.com>
2025-12-19 17:54:31 +0900

    fix minjoon 4

--- start.sh
+++ start.sh
@@ -3,21 +3,12 @@ set -e
 
 APP_DIR=/home/ec2-user/app
 LOG_FILE=$APP_DIR/app.log
+JAR_FILE=$APP_DIR/app.jar
 
 cd $APP_DIR
 
-touch "$LOG_FILE"
-chmod 644 "$LOG_FILE"
-
-JAR_FILE=$(ls -1 build/libs/*.jar | head -n 1)
-
-if [ -z "$JAR_FILE" ]; then
-  echo "❌ JAR not found in build/libs" >> "$LOG_FILE"
-  exit 1
-fi
-
-echo "▶ Starting $JAR_FILE" >> "$LOG_FILE"
-nohup java -jar "$JAR_FILE" >> "$LOG_FILE" 2>&1 &
+echo "▶ Starting app.jar"
+nohup java -jar "$JAR_FILE" > "$LOG_FILE" 2>&1 &
 
 
 
