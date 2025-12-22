03266549d4e0f570ad9d4935c7d6846ca583e60a
Y9957 <dbscodk9957@gmail.com>
2025-12-23 06:22:18 +0900

    test1

--- src/main/java/com/example/demo/controller/HealthController.java
+++ src/main/java/com/example/demo/controller/HealthController.java
@@ -1,3 +1,8 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HealthController {
 
